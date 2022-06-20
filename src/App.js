73bb49827ee8a39f11076babb80f58834eabe5dc
linsean <linsean@LindeAir>
2022-06-20 11:15:07 +0800

    make reactApp transform to myTodo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="workSpace">
+        <div className="bg_navbar">
+            <div className="navContainer">   
+            <div className="filter">My Tasks</div>
+            <div className="filter">In Progress</div>
+            <div className="filter">Completed</div> 
+        </div>
+
+        <div className="input">
+            <input className="inputer" placeholder=" + Add Task"/>
+        </div>
+
+    <div className="todoList">
+
+        <div className="todoItem">
+
+            <div className="todoCheckbox">
+                <div className="checkLabel">
+                <input type="checkbox" id="checkbox"/>
+                <label for="checkbox">Type Something Here…</label> 
+                </div>
+                  
+                <div className="todoBtn">
+            <button className="btn_star">＃</button>
+            <button className="btn_write">＃</button>
+                </div>
+            </div>
+                <div className="todoData">
+                <button className="date">＃</button>
+                <button className="detailDate">＃</button>
+                <button className="file">＃</button>
+                <button className="comment">＃</button>
+            </div>
+        </div>
+
+            <div className="checkedTodoItem">
+                <div className="todoCheckbox">
+                    <div className="checkLabel">
+                    <input type="checkbox" id="checkbox"/>
+                    <label for="checkbox">Type Something Here…</label> 
+                    </div>
+                      
+                    <div className="todoBtn">
+                <button className="btn_star">＃</button>
+                <button className="btn_write">＃</button>
+                    </div>
+                </div>
+                    <div className="todoData">
+                    <button className="date">＃</button>
+                    <button className="detailDate">＃</button>
+                    <button className="file">＃</button>
+                    <button class="comment">＃</button>
+                </div>
+            </div>
+    
+        </div>
+    </div>
+    </div>
     </div>
   );
 }
